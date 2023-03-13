6c4a5d83d28fbd8c143acfc75ea123d74c6ba2bb
Paventoon <pashanovikov3333@mail.ru>
2023-03-13 22:05:55 +0300

    Add files via upload

---
+++ ze.cs
@@ -0,0 +1,40 @@
+int[,,] array3D = new int[2, 2, 2];
+FillArray(array3D);
+PrintIndex(array3D);
+
+
+ 
+void PrintIndex(int[,,] arr)
+{
+    for    (int i=0;)   
+    {
+        for    (int j=0;)   
+        {
+            Console.WriteLine();
+            for    (int k=0;)   
+            {
+                Console.Write($"{array3D[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+
+void FillArray(int[,,] arr)
+{
+    array = Random(1,10).Next;
+    for    (int i=0;)   
+    {
+        for    (int  j=0;) 
+        {
+            for    (int k=0;) 
+            {
+                arr[k, i, j] += count;
+                void arr = 3 += 3;
+                return;
+            }
+        }
+    }
+}
+
+ 
